080afc72fc22f5886a1b03863a3e9f3e34c49bc1
Bibek Gaihre <bibek.gaihre@rumsan.com>
2019-03-23 19:50:26 +0545

    user module created

---
+++ modules/user/user.model.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const { ObjectId } = mongoose.Types;
+
+const UsersSchema = mongoose.Schema(
+  {
+    name: {
+      first_name: {
+        type: String,
+        required: true
+      },
+      last_name: {
+        type: String,
+        required: true
+      }
+    },
+    password: {
+      hash: {
+        type: String,
+        required: true
+      },
+      salt: String
+    },
+    is_active: { type: Boolean, required: true, default: true },
+    comms: [{ type: ObjectId, ref: "User_comm" }],
+    roles: [
+      {
+        type: String,
+        $in: ["frontdesk", "food_operator", "housekeeper", "bellman"],
+        default: "user"
+      }
+    ]
+  },
+  {
+    collection: "user",
+    timeStamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    },
+    toObject: {
+      virtuals: true
+    },
+    toJson: {
+      virtuals: true
+    }
+  }
+);
+
+module.exports = mongoose.model("User", UsersSchema);
+
+UsersSchema.virtual("name.full").get(() => {
+  return this.name.first_name + " " + this.name.last_name;
+});
+
+UsersSchema.virtual("email").get(() => {
+  try {
+    if (this.comms.length == 0) return null;
+
+    let email = this.comms.find(e => {
+      return e.type === "email" && e.is_primary;
+    });
+    if (email) return email.address;
+
+    email = this.comms.find(e => {
+      return e.type === "email";
+    });
+    if (email) return email.address;
+  } catch (e) {
+    return e;
+  }
+});
